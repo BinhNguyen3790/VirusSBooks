8a5e05b1243e1ab9c6e006249b3e6b9ad6830949
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2019-02-28 20:13:17 +0700

    modify dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -5,10 +5,10 @@
  * Date: 26/01/2019
  * Time: 8:43 AM
  */
-  $host = "localhost";
-  $name = "root";
-  $password = "";
-  $dbname = "vsbooks";
+  $host = "ec2-54-243-128-95.compute-1.amazonaws.com";
+  $name = "mhccfxpsotauoa";
+  $password = "b1161b2943861aa68636c64bea12b6eb9abe91559adfddeb75e5daf4847adcdd";
+  $dbname = "d3cgrg8ca4irte";
   $dbc = mysqli_connect($host, $name, $password, $dbname)
     or die("connect error:" . mysqli_connect_error());
 ?>
